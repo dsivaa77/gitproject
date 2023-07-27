1d52cd1f934ae2d22033dd8963f5899606f10272
Siva <Siva@192.168.3.221>
2023-07-27 15:28:44 +0530

    fifth commit

---
+++ gitproject/src/test/java/Testgitproject/gitproject/gitmainclass.java
@@ -0,0 +1,20 @@
+package Testgitproject.gitproject;
+
+import org.junit.Test;
+
+//Sivaramakrishna
+
+public class gitmainclass {
+	
+    @Test
+	public void maventest(){
+	   
+		System.out.println("This is git maven project");
+
+	}
+    @Test
+    public void maventest1() {
+    	System.out.println("This is git maven project");
+    }
+
+}
